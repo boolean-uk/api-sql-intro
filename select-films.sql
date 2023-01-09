8f3b62986712094992fb7730afee0269029325f6
Fatima <fxtimaahmed14@gmail.com>
2023-01-09 15:21:14 +0000

    14/14 SELECT statements written

--- select-films.sql
+++ select-films.sql
@@ -32,3 +32,25 @@ WHERE release_year > 1990
 -- films released between 1990 and 1999
 SELECT * FROM films
 WHERE release_year BETWEEN 1990 AND 1999
+
+-- films with the genre of "SciFi"
+SELECT * FROM films
+WHERE genre = 'SciFi'
+
+-- films with the genre of "Western" or "SciFi"
+SELECT * FROM films
+WHERE genre = 'SciFi'
+OR  genre = 'Western'
+
+-- films with any genre _apart_ from "SciFi"
+SELECT * FROM films
+WHERE NOT genre = 'SciFi'
+
+-- films with the genre of "Western" released before 2000
+SELECT * FROM films
+WHERE genre = 'Western'
+AND release_year < 2000
+
+-- films that have the world "Matrix" in their title
+SELECT * FROM films
+WHERE title LIKE '%Matrix%'
