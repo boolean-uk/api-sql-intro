28b1b1e29bc996c1e1c83141584de82bc8123b87
akibria12 <ahkibria@hotmail.co.uk>
2023-01-10 00:31:17 +0000

    final

--- sql/films/insert-films.sql
+++ sql/films/insert-films.sql
@@ -1,22 +1,15 @@
-SELECT INTO films(title,genre,release_year, score)
-VALUES ('The Shawshank Redemtion', 'Drama',1994,9),
-('The Godfather','Crime',1952,9),
-('The dark knight','Action',2008,9),
-('Alien', 'SciFi', 1979, 9),
-('Total Recall', 'SciFi', 1990, 8),
-('The Matrix', 'SciFi', 1999, 8),
-('The Matrix Resurrections', 'SciFi', 2021, 5),
-('The Matrix Reloaded', 'SciFi', 2003, 6),
-('The Hunt for Red October', 'Thriller', 1990, 7),
-('Misery', 'Thriller', 1990, 7),
-('The Power Of The Dog', 'Western', 2021, 6),
-('Hell or High Water', 'Western', 2016, 8),
-('The Good the Bad and the Ugly', 'Western', 1966, 9),
-('Unforgiven', 'Western', 1992, 7)
-
-
-
-
-
-
-
+insert into films(title,genre,release_year, score, director_id)
+values ('The Shawshank Redemtion', 'Drama',1994,9,1),
+('The Godfather','Crime',1952,9,1),
+('The dark knight','Action',2008,9,1),
+('Alien', 'SciFi', 1979, 9,2),
+('Total Recall', 'SciFi', 1990, 8,2),
+('The Matrix', 'SciFi', 1999, 8,2),
+('The Matrix Resurrections', 'SciFi', 2021, 5,2),
+('The Matrix Reloaded', 'SciFi', 2003, 6,3),
+('The Hunt for Red October', 'Thriller', 1990, 7,4),
+('Misery', 'Thriller', 1990, 7,4),
+('The Power Of The Dog', 'Western', 2021, 6,4),
+('Hell or High Water', 'Western', 2016, 8,5),
+('The Good the Bad and the Ugly', 'Western', 1966, 9,5),
+('Unforgiven', 'Western', 1992, 7,6)
