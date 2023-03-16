04443556dcaefa14a53d1f121c890580582d0737
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-03-16 11:49:03 +0000

    insert film data

--- sql/core/new table.sql
+++ sql/core/new table.sql
@@ -2,7 +2,7 @@ CREATE TABLE films (
     id SERIAL PRIMARY KEY,
     title VARCHAR(32) NOT NULL,
     genre VARCHAR(32) NOT NULL,
-    release INTEGER NOT NULL,
+    release_year INTEGER NOT NULL,
     score INTEGER DEFAULT 0,
     UNIQUE(title)
 )
