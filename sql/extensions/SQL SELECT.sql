10bf605b87fe290b1951332a308f8cb54a60ce9e
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-03-16 12:32:11 +0000

    extensions 2 and 3

--- sql/extensions/SQL SELECT.sql
+++ sql/extensions/SQL SELECT.sql
@@ -7,3 +7,11 @@ SELECT COUNT(*) AS "Amount of films" FROM films;
 SELECT genre, AVG(score) AS "Average score of genre" FROM films 
 GROUP BY genre
 ORDER BY "Average score of genre" DESC;
+
+-- https://www.w3schools.com/sql/sql_join.asp
+SELECT films.title AS "Film title", directors.name AS "Director name" FROM films
+INNER JOIN directors ON directors.id = films.director_id;
+
+SELECT COUNT(films.id) AS "Number of films", directors.name AS "Director name" FROM films
+INNER JOIN directors ON directors.id = films.director_id
+GROUP BY directors.name;
---
+++ sql/extensions/insert directors.sql
@@ -0,0 +1,7 @@
+INSERT INTO directors (name)
+VALUES 
+('Ridley Scott'), 
+('Francis Ford Coppola'), 
+('Christopher Nolan'),
+('Frank Darabont'), 
+('Jane Campion');
