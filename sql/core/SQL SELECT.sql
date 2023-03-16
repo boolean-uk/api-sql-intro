c7ce8b18e816a45b9b223c4696285d619b377385
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-03-16 12:04:12 +0000

    extension 1

--- sql/core/SQL SELECT.sql
+++ sql/core/SQL SELECT.sql
@@ -37,5 +37,14 @@ SELECT * FROM films
 WHERE genre = 'Western' AND release_year < 2000;
 
 SELECT * FROM films
-WHERE title LIKE '%Matrix%'
+WHERE title LIKE '%Matrix%';
 
+-- https://www.w3schools.com/sql/func_sqlserver_avg.asp
+SELECT AVG(score) AS "Average film score" FROM films;
+
+-- https://www.w3schools.com/sql/func_sqlserver_count.asp
+SELECT COUNT(*) AS "Amount of films" FROM films;
+
+SELECT genre, AVG(score) AS "Average score of genre" FROM films 
+GROUP BY genre
+ORDER BY "Average score of genre" DESC;
