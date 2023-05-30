ec5d73f5a12af59da25eb407acfabf49c3784fba
brightWin <konstantinos.vroustouris@gmail.com>
2023-05-30 15:47:55 +0300

    added descriptive comments in select-films.sql and extension-1.sql files

--- sql/films/extension-1.sql
+++ sql/films/extension-1.sql
@@ -1,3 +1,8 @@
+--Return the average film rating
 SELECT AVG(score) FROM films;
+
+--Return the total number of films
 SELECT COUNT(*) FROM films;
+
+--Return the average film rating by genre
 SELECT AVG(score) FROM films GROUP BY genre;
