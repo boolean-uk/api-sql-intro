2ee41a5f4b67b63bfd50340cfc46a66e2fdb8510
PanagiotisTziolos <panagiotistziolos@outlook.com>
2023-05-30 14:26:09 +0300

    almost done

---
+++ sql/products/standard_criteria.sql
@@ -0,0 +1,38 @@
+create table if not exists films (
+    id serial primary key,
+    title varchar(50) unique,
+    genre varchar(50),
+    release_year integer,
+    score integer
+);
+
+insert into films 
+(title, genre, release_year, score) values
+('The Shawshank Redemption', 'Drama', 1994, 9),
+('The Godfather', 'Crime', 1972, 9),
+('The Dark Knight', 'Action', 2008, 9),
+('Alien', 'SciFi', 1979, 9),
+('Total Recall', 'SciFi', 1990, 8),
+('The Matrix', 'SciFi', 1999, 8),
+('The Matrix Resurrections', 'SciFi', 2021, 5),
+('The Matrix Reloaded', 'SciFi', 2003, 6),
+('The Hunt for Red October', 'Thriller', 1990, 7),
+('Misery', 'Thriller', 1990, 7),
+('The Power Of The Dog', 'Western', 2021, 6),
+('Hell or High Water', 'Western', 2016, 8),
+('The Good the Bad and the Ugly', 'Western', 1966, 9),
+('Unforgiven', 'Western', 1992, 7)
+
+select * from films;
+
+select * from films order by score desc;
+
+select * from films order by release_year asc;
+
+select * from films where score >= 8;
+
+select * from films where score <= 7;
+
+select * from films where release_year = 1990;
+
+select * from films where release_year < 2000;
