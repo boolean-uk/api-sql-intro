420b18fd8910d8403280d518e01ddf101670691f
Rose Breedveld <rosebreedveld@hotmail.com>
2023-05-30 08:50:39 -0400

    added the questions of the ReadMe file

--- sql/products/standard_criteria.sql
+++ sql/products/standard_criteria.sql
@@ -23,38 +23,48 @@ insert into films
 ('The Good the Bad and the Ugly', 'Western', 1966, 9),
 ('Unforgiven', 'Western', 1992, 7)
 
+--All films
 select * from films;
 
+--All films ordered by rating descending
 select * from films order by score desc;
 
+--All films ordered by release year ascending
 select * from films order by release_year asc;
 
+--All films with a rating of 8 or higher
 select * from films where score >= 8;
 
+--All films with a rating of 7 or lower
 select * from films where score <= 7;
 
+--films released in 1990
 select * from films where release_year = 1990;
 
+--films released before 2000
 select * from films where release_year < 2000;
 
+--films released after 1990
 select * from films where release_year > 1990;
 
+--films released between 1990 and 1999
 select * from films where release_year between 1990 and 1999;
 
+--films with the genre of "SciFi"
 select * from films where genre = 'SciFi';
 
+--films with the genre of "Western" or "SciFi"
 select * from films where genre = 'SciFi' OR genre = 'Western';
 select * from films where genre in ('SciFi', 'Western');
 
+--films with any genre apart from "SciFi"
 select * from films where genre not like 'SciFi';
 
+--films with the genre of "Western" released before 2000
 select * from films where release_year < 2000 and genre like 'Western';
 
+--films that have the world "Matrix" in their title
 select * from films where title like '%Matrix%';
 
-select avg(score) from films;
 
-select COUNT(id) from films;
-
-select genre, avg(score) as score from films group by genre;
 
