b126684b1e809f92238def477f22393376a524af
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-05-30 13:28:04 +0200

    first part of standard criteria done

---
+++ sql/standard/selectfilms.sql
@@ -0,0 +1,16 @@
+select * from films;
+
+select * from films order by score desc;
+
+select * from films order by releaseyear asc
+
+select * from films where score > 7
+
+select * from films where score < 8
+
+select * from films where releaseyear = 1990
+
+select * from films where releaseyear < 2000
+
+select * from films where releaseyear > 1990
+
