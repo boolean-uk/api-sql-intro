49b9287fd52de6f9b1529959c31c687f19661fc6
Sebastian Andrisan <sebyyyandr@gmail.com>
2023-05-30 14:41:07 +0300

    standard criteria: complete

--- sql/standard/selectfilms.sql
+++ sql/standard/selectfilms.sql
@@ -14,3 +14,14 @@ select * from films where releaseyear < 2000
 
 select * from films where releaseyear > 1990
 
+select * from films where releaseyear between 1990 and 1999
+
+select * from films where genre = 'SciFi'
+
+select * from films where genre = 'SciFi' or genre = 'Western'
+
+select * from films where NOT genre = 'SciFi'
+
+select * from films where genre = 'Western' and releaseyear < 2000
+
+select * from films where title LIKE '%Matrix%'
