646a758248eda3df6e12aab044165f02f5a967e1
Mantas Navickis <navickis@gmail.com>
2023-06-29 18:34:43 +0200

    updated ext3

--- sql/films/extensions_3.sql
+++ sql/films/extensions_3.sql
@@ -1,4 +1,4 @@
-SELECT directors.directorname, COUNT(films.directorid)
+SELECT directors.director_name, COUNT(films.director_id)
 FROM directors
-JOIN films ON directors.directorid = films.directorid
-GROUP BY directors.directorname
+JOIN films ON directors.director_Id = films.director_id
+GROUP BY directors.director_name
