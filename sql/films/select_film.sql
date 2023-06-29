531f2d3e82b6e4e66e9d3b77dc68742dbb453bb0
Sara Beazley <sarajbeazley@gmail.com>
2023-06-29 15:59:13 -0700

    core completed

--- sql/films/select_film.sql
+++ sql/films/select_film.sql
@@ -4,4 +4,23 @@ SELECT * FROM films ORDER BY score DESC;
 
 SELECT * FROM films ORDER BY release_year ASC;
 
-SELECT * FROM films WHERE SCORE > 8;
+SELECT * FROM films WHERE SCORE >= 8;
+
+SELECT * FROM films WHERE SCORE <= 7;
+
+SELECT * FROM films WHERE release_year = 1990;
+
+SELECT * FROM films WHERE release_year <= 2000;
+
+SELECT * FROM films WHERE release_year BETWEEN 1990 AND 1999;
+
+SELECT * FROM films WHERE genre = 'SciFi';
+
+SELECT * FROM films WHERE genre = 'SciFi' OR genre = 'Western';
+
+SELECT * FROM films WHERE genre != 'SciFi';
+
+SELECT * FROM films WHERE genre = 'Western' AND release_year <= 2000;
+
+SELECT * FROM films WHERE title LIKE '%Matrix%';
+
