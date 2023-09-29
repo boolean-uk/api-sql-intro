12c64971fbfe6b36870a2778595f6299a2f6822b
cmdalikeskin <116778628+cmdalikeskin@users.noreply.github.com>
2023-09-29 22:41:23 +0200

    Added comments

--- Core exercises.sql
+++ Core exercises.sql
@@ -1,3 +1,4 @@
+-- CREATING THE TABLE 'FILMS'
 CREATE TABLE IF NOT EXISTS films (
     id SERIAL PRIMARY KEY,
     title TEXT,
@@ -6,6 +7,8 @@ CREATE TABLE IF NOT EXISTS films (
     score INTEGER
 );
 
+
+-- ADDING VALUES TO THE FILMS TABLE
 INSERT INTO films
 (title, genre, release_year, score)
 VALUES
@@ -24,66 +27,59 @@ VALUES
 ('The Good the Bad and the Ugly', 'Western', 1966, 9),
 ('Unforgiven', 'Western', 1992, 7);
 
-
-
-
-
-
-
-Write SQL SELECT statements to return films matching the below criteria:
-* All films
+-- THE CORE EXERCISES ARE FOUND BELOW
+-- All films
 SELECT * FROM films;
 
-* All films ordered by rating descending
+-- All films ordered by rating descending
 SELECT * FROM films
 ORDER BY score DESC;
 
-* All films ordered by release year ascending
+-- All films ordered by release year ascending
 SELECT * FROM films
 ORDER BY release_year ASC;
 
-* All films with a rating of 8 or higher
+-- All films with a rating of 8 or higher
 SELECT * FROM films
 WHERE score >= 8;
 
-* All films with a rating of 7 or lower
+-- All films with a rating of 7 or lower
 SELECT * FROM films
 WHERE score <= 7;
 
-* films released in 1990
+-- films released in 1990
 SELECT * FROM films
 WHERE release_year = 1990;
 
-* films released before 2000
-	SELECT * FROM films
-	WHERE release_year < 2000;
+-- films released before 2000
+SELECT * FROM films
+WHERE release_year < 2000;
 
-* films released after 1990
-	SELECT * FROM films
-	WHERE release_year > 1990;
+-- films released after 1990
+SELECT * FROM films
+WHERE release_year > 1990;
 
+-- films released between 1990 and 1999
+SELECT * FROM films
+WHERE release_year BETWEEN 1990 AND 1999;
 
-* films released between 1990 and 1999
-	SELECT * FROM films
-	WHERE release_year BETWEEN 1990 AND 1999;
+-- films with the genre of 'SciFi'
+SELECT * FROM films
+WHERE genre = 'SciFi';
 
-* films with the genre of 'SciFi'
-	SELECT * FROM films
-	WHERE genre = 'SciFi';
+-- films with the genre of 'Western' or 'SciFi'
+SELECT * FROM films
+WHERE genre = 'Western' OR genre = 'SciFi';
 
-* films with the genre of 'Western' or 'SciFi'
-    SELECT * FROM films
-    WHERE genre = 'Western' OR genre = 'SciFi';
-
-* films with any genre *apart* from 'SciFi'
+-- films with any genre *apart* from 'SciFi'
 SELECT * FROM films
 WHERE genre != 'SciFi';
 
-* films with the genre of 'Western' released before 2000
+-- films with the genre of 'Western' released before 2000
 SELECT * FROM films
 WHERE genre = 'Western' AND release_year <2000;
 
-* films that have the world 'Matrix' in their title
+-- films that have the world 'Matrix' in their title
 SELECT * FROM films
 WHERE title LIKE '%Matrix%';
 
