1261e2ad27a4488e515bd93363193c03d551883f
cmdalikeskin <116778628+cmdalikeskin@users.noreply.github.com>
2023-09-29 21:49:30 +0200

    First

--- sql/products/insert-products.sql
+++ sql/products/insert-products.sql
@@ -2,3 +2,13 @@ insert into products(name, price, discount) VALUES ('apple', 135, false);
 insert into products(name, price, discount) VALUES ('orange', 55, true);
 insert into products(name, price, discount) VALUES ('kiwi', 165, true);
 insert into products(name, price, discount) VALUES ('banana', 035, false);
+
+
+
+CREATE TABLE IF NOT EXISTS films (
+id SERIAL PRIMARY KEY,
+    title TEXT,
+    genre TEXT,
+    release_year INTEGER,
+    score INTEGER
+    );
