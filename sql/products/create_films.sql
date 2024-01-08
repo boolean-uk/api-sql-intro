b8c075be70f3427d7c518598276a7487ac03f756
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-08 15:42:40 +0100

    add create table and insert tabel

---
+++ sql/products/create_films.sql
@@ -0,0 +1,9 @@
+CREATE TABLE films (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL UNIQUE,
+  genre VARCHAR(255),
+  release_year INT,
+  check(release_year >= 1800 and release_year <= 2200),
+  score INT,
+  check(score BETWEEN 0 AND 10)
+);
