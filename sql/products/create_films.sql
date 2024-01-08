56895b77fdd546cd821959f8252223e64ffc7a9b
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-08 17:39:03 +0100

    add directors_id to movies

--- sql/products/create_films.sql
+++ sql/products/create_films.sql
@@ -5,5 +5,7 @@ CREATE TABLE films (
   release_year INT,
   check(release_year >= 1800 and release_year <= 2200),
   score INT,
-  check(score BETWEEN 0 AND 10)
+  check(score BETWEEN 0 AND 10),
+  director_id INT,
+  check(director_id > 0)
 );
---
+++ sql/products/extensions-2.sql
@@ -0,0 +1,23 @@
+ALTER TABLE films
+ADD director_id INT;
+
+CREATE TABLE directors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+
+INSERT INTO directors (name)
+  VALUES('Frank Darabont'),
+  ('Francis Ford Coppola'),
+  ('Christopher Nolan'),
+  ('Ridley Scott'),
+  ('Paul Verhoeven'),
+  ('The Wachowskis'),
+  ('Lana Wachowski'),
+  ('John McTiernan'),
+  ('Rob Reiner'),
+  ('Jane Campion'),
+  ('David Mackenzie'),
+  ('Sergio Leone'),
+  ('Clint Eastwood');
+
