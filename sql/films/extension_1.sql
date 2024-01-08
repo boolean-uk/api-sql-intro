9d149a1df5feeb913ecaf84e73a00c6e8a112299
ilham saleh <ilham2saleh@gmail.com>
2024-01-08 18:59:28 +0000

    Update Extension 1

--- sql/films/extension_1.sql
+++ sql/films/extension_1.sql
@@ -1,8 +1,10 @@
 -- Return the average film rating
 SELECT AVG(score)::NUMERIC(10, 2) AS average_rateing from films
 
+SELECT CAST(AVG(score)AS INTEGER) as average_score FROM films;
+
 -- Return the total number of films
 SELECT COUNT(*) as total_films from films
 
 -- Return the average film rating by genre
-SELECT genre, AVG(score) FROM films GROUP BY genre
+SELECT genre, CAST(AVG(score)AS INTEGER) FROM films GROUP BY genre
