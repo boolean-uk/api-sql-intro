62da03e2286617e9a6470163c7e8466a75058d80
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-01-29 16:12:18 +0100

    Finished Core

---
+++ sql_intro.sql
@@ -0,0 +1,66 @@
+CREATE TABLE films (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255) UNIQUE,
+	genre VARCHAR(50),
+	release_year INT,
+	rating INT
+);
+
+--Inserting initial data
+INSERT INTO films (name, genre, release_year, rating) VALUES
+('The Shawshank Redemption', 'Drama', 1994, 9),
+('The Godfather', 'Crime', 1972, 9),
+('The Dark Knight', 'Action', 2008, 9),
+('Alien', 'SciFi', 1979, 9),
+('Total Recall', 'SciFi', 1990, 8),
+('The Matrix', 'SciFi', 1999, 8),
+('The Matrix Resurrections', 'SciFi', 2021, 5),
+('The Matrix Reloaded', 'SciFi', 2003, 6),
+('The Hunt for Red October', 'Thriller', 1990, 7),
+('Misery', 'Thriller', 1990, 7),
+('The Power Of The Dog', 'Western', 2021, 6),
+('Hell or High Water', 'Western', 2016, 8),
+('The Good the Bad and the Ugly', 'Western', 1966, 9),
+('Unforgiven', 'Western', 1992, 7)
+
+--QUERIES
+--All films
+SELECT * FROM films;
+
+--All films ordered by rating desc
+SELECT * FROM films ORDER BY rating DESC;
+
+--All films ordered by release year asc
+SELECT * FROM films ORDER BY release_year ASC;
+
+--All films with a rating of 8 or higher
+SELECT * FROM films WHERE rating >= 8;
+
+-- All films with a rating of 7 or lower
+SELECT * FROM films WHERE rating < 8;
+
+-- films released in 1990
+SELECT * FROM films WHERE release_year = 1990;
+
+-- films released before 2000
+SELECT * FROM films WHERE release_year < 2000;
+
+-- films released after 1990
+SELECT * FROM films WHERE release_year > 1990;
+
+-- films released between 1990 and 1999
+SELECT * FROM films WHERE release_year >= 1990 AND release_year <= 1999;
+-- films with the genre of "SciFi"
+SELECT * FROM films WHERE genre LIKE 'SciFi';
+
+-- films with the genre of "Western" or "SciFi"
+SELECT * FROM films WHERE genre LIKE 'Western' OR genre LIKE 'SciFi';
+
+-- films with any genre apart from "SciFi"
+SELECT * FROM films WHERE genre NOT LIKE 'SciFi';
+
+-- films with the genre of "Western" released before 2000
+SELECT * FROM films WHERE genre LIKE 'Western' AND release_year < 2000;
+
+-- films that have the world "Matrix" in their title
+SELECT * FROM films WHERE name LIKE 'Matrix';
