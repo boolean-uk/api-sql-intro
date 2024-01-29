8f19441e636584e87283b35dd100c9918e304eda
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-29 14:35:29 +0100

    Minor output refactor

--- sql/films/Extension1.sql
+++ sql/films/Extension1.sql
@@ -1,15 +1,15 @@
 -- Return the average film rating
-SELECT AVG(score)
+SELECT Round(AVG(score), 4) as avg_score
 FROM films
 ;
 
 -- Return the total number of films
-SELECT COUNT(title)
+SELECT COUNT(title) as number_of_movies
 FROM films
 ;
 
 -- Return the average film rating by genre
-SELECT genre, Round(AVG(score), 2)
+SELECT genre, Round(AVG(score), 2) as avg_score
 FROM films
 GROUP BY (genre)
 ORDER BY AVG(score) DESC
