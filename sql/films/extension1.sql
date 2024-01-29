39cf7d5e8f9c930d2067db3938d96242ebb52374
Gudbrand <gudbrandwd@live.no>
2024-01-29 12:10:48 +0100

    extension1 done

---
+++ sql/films/extension1.sql
@@ -0,0 +1,6 @@
+--Return the average film rating
+SELECT AVG(score) FROM films
+--Return the total number of films
+SELECT COUNT(*) FROM films
+--Return the average film rating by genre
+SELECT genre, AVG(score) FROM films GROUP BY genre
