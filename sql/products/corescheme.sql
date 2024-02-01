005462b989170ddd8f099a69e3c709235368b250
tonnesegeland <to.smith.eg@gmail.com>
2024-02-01 15:32:09 +0100

    extensions done!

--- sql/products/corescheme.sql
+++ sql/products/corescheme.sql
@@ -8,22 +8,22 @@ CREATE TABLE films (
 	Score INT
 );
 
-INSERT INTO films (Id, Title, Genre, Year, Score)
+INSERT INTO films (Title, Genre, Year, Score)
 VALUES
-(1, 'The Shawshank Redemption', 'Drama', 1994, 9),
-(2, 'The Godfather', 'Crime', 1972, 9),
-(3, 'The Dark Knight', 'Action', 2008, 9),
-(4, 'Alien', 'SciFi', 1979, 9),
-(5, 'Total Recall', 'SciFi', 1990, 8),
-(6, 'The Matrix', 'SciFi', 1999, 8),
-(7, 'The Matrix Resurrections', 'SciFi', 2021, 5),
-(8, 'The Matrix Reloaded', 'SciFi', 2003, 6),
-(9, 'The Hunt for Red October', 'Thriller', 1990, 7),
-(10, 'Misery', 'Thriller', 1990, 7),
-(11, 'The Power Of The Dog', 'Western', 2021, 6),
-(12, 'Hell or High Water', 'Western', 2016, 8),
-(13, 'The Good the Bad and the Ugly', 'Western', 1966, 9),
-(14, 'Unforgiven', 'Western', 1992, 7);
+('The Shawshank Redemption', 'Drama', 1994, 9),
+('The Godfather', 'Crime', 1972, 9),
+('The Dark Knight', 'Action', 2008, 9),
+('Alien', 'SciFi', 1979, 9),
+('Total Recall', 'SciFi', 1990, 8),
+('The Matrix', 'SciFi', 1999, 8),
+('The Matrix Resurrections', 'SciFi', 2021, 5),
+('The Matrix Reloaded', 'SciFi', 2003, 6),
+('The Hunt for Red October', 'Thriller', 1990, 7),
+('Misery', 'Thriller', 1990, 7),
+('The Power Of The Dog', 'Western', 2021, 6),
+('Hell or High Water', 'Western', 2016, 8),
+('The Good the Bad and the Ugly', 'Western', 1966, 9),
+('Unforgiven', 'Western', 1992, 7);
 
 
 
