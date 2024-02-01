1a7bb7c5d4c48470d5212dcfebe29fc3d0d455bd
Sebastian <sebastian.thunberg@hotmail.se>
2024-02-01 13:56:00 +0100

    Sebastian Thunberg

--- sql/products/extension.sql
+++ sql/products/extension.sql
@@ -1,3 +1,27 @@
+-- Create table directors
+CREATE TABLE directors (
+    directorId SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+--Fill table with data
+INSERT INTO films (title, genre, release_year, score, directorId) VALUES
+('The Shawshank Redemption', 'Drama', 1994, 9, 1),
+('The Godfather', 'Crime', 1972, 9, 2),
+('The Dark Knight', 'Action', 2008, 9, 3),
+('Alien', 'SciFi', 1979, 9, 1),
+('Total Recall', 'SciFi', 1990, 8, 2),
+('The Matrix', 'SciFi', 1999, 8, 3),
+('The Matrix Resurrections', 'SciFi', 2021, 5, 1),
+('The Matrix Reloaded', 'SciFi', 2003, 6, 2),
+('The Hunt for Red October', 'Thriller', 1990, 7, 3),
+('Misery', 'Thriller', 1990, 7, 1),
+('The Power Of The Dog', 'Western', 2021, 6, 2),
+('Hell or High Water', 'Western', 2016, 8, 3),
+('The Good the Bad and the Ugly', 'Western', 1966, 9, 1),
+('Unforgiven', 'Western', 1992, 7, 2);
+
+
 -- 1. Return the average film rating
 SELECT AVG(score) AS average_rating FROM films;
 
