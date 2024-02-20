d685e9951629c8299b7085ec3fcc0a9d42aa6c60
Rose van der Giessen <Rose van der Giessen>
2024-02-20 13:31:26 +0100

    Redid exercise for c# course

--- sql/directors/create-directors.sql
+++ sql/directors/create-directors.sql
@@ -2,14 +2,3 @@ CREATE TABLE directors(
  id serial primary key,
  name varchar(80) not null
 );
-
-CREATE TABLE films(
-  id serial primary key,
-  title varchar(80) not null,
-  genre varchar(50),
-  release_year INTEGER,
-  score INTEGER,
-  director_id INTEGER,
-  foreign key (director_id) references directors(id),
-  unique(title)
-);
