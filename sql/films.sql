c94eb785d0815a96d11088379aef1f1725b89bfa
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-20 12:02:07 +0200

    add director table and join with films

--- sql/films.sql
+++ sql/films.sql
@@ -43,3 +43,52 @@ SELECT AVG(rating) FROM films;
 SELECT COUNT(*) FROM films;
 SELECT genre, AVG(rating) FROM films GROUP BY genre;
 
+CREATE TABLE directors(
+ id serial primary key,
+ name TEXT
+);
+
+BEGIN;
+INSERT INTO directors
+(name) 
+VALUES 
+('Jane Doe'),
+('Johny Something'),
+('Unkown Person'),
+('Real Director'),
+('Unreal Director') RETURNING *;
+COMMIT;
+
+DROP TABLE films;
+
+CREATE TABLE films(
+ id serial primary key,
+ title TEXT UNIQUE,
+ genre TEXT,
+ release_year INTEGER,
+ rating INTEGER,
+ director_id INTEGER, 
+ FOREIGN KEY (director_id) REFERENCES directors(id)
+);
+
+BEGIN;
+INSERT INTO films
+(title, genre, release_year, rating, director_id) 
+VALUES 
+('The Shawshank Redemption', 'Drama', 1994, 9, 1),
+('The Godfather', 'Crime', 1972, 9, 5),
+('The Dark Knight', 'Action', 2008, 9, 2),
+('Alien', 'SciFi', 1979, 9, 4),
+('Total Recall', 'SciFi', 1990, 8, 3),
+('The Matrix', 'SciFi', 1999, 8, 3),
+('The Matrix Resurrections', 'SciFi', 2021, 5, 1),
+('The Matrix Reloaded', 'SciFi', 2003, 6, 5),
+('The Hunt for Red October', 'Thriller', 1990, 7, 5),
+('Misery', 'Thriller', 1990, 7, 5),
+('The Power Of The Dog', 'Western', 2021, 6, 2),
+('Hell or High Water', 'Western', 2016, 8, 4),
+('The Good the Bad and the Ugly', 'Western', 1966, 9, 3),
+('Unforgiven', 'Western', 1992, 7, 2) RETURNING *;
+COMMIT;
+
+SELECT * FROM films INNER JOIN directors ON films.director_id = directors.id;
