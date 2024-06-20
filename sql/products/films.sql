fd57cc45d4f0914a94f6ffc1412eea55386a9608
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-06-20 19:23:04 +0200

    extension 2

--- sql/products/films.sql
+++ sql/products/films.sql
@@ -1,5 +1,5 @@
 -- STANDARD
--- Create the films table.
+-- create the films table.
  CREATE TABLE IF NOT EXISTS films(
   id SERIAL PRIMARY KEY,
   title TEXT UNIQUE NOT NULL,
@@ -8,7 +8,7 @@
   score INTEGER
 )
 
--- Adding records in to the films table.
+-- adding records in to the films table.
 INSERT INTO films
        (title, genre, release_year, score)
 VALUES ('The Shawshank Redemption', 'Drama', 1994, 9),
@@ -26,28 +26,28 @@ VALUES ('The Shawshank Redemption', 'Drama', 1994, 9),
        ('The Good the Bad and the Ugly', 'Western', 1966, 9),
        ('Unforgiven', 'Western', 1992, 7);
 
-
--- Select all films
+-- QUERIES
+-- select all films
 SELECT * 
 FROM films;
 
 
--- All films ordered by rating descending
+-- all films ordered by rating descending
 SELECT * 
 FROM films 
 ORDER BY score DESC
 
--- All films ordered by release year ascending
+-- all films ordered by release year ascending
 SELECT * 
 FROM films 
 ORDER BY release_year ASC
 
--- All films with a rating of 8 or higher
+-- all films with a rating of 8 or higher
 SELECT * 
 FROM films 
 WHERE score >= 8
 
--- All films with a rating of 7 or lower
+-- all films with a rating of 7 or lower
 SELECT * 
 FROM films 
 WHERE score <= 7
@@ -110,3 +110,59 @@ FROM films
 SELECT ROUND(AVG(score)) AS avg_rating_by_genre
 FROM films
 GROUP BY genre;
+
+-- EXTENSION 2
+-- create a new directors table
+ CREATE TABLE IF NOT EXISTS directors(
+  id SERIAL PRIMARY KEY,
+  name TEXT 
+)
+
+-- adding records in to the diersctors table
+INSERT INTO films
+       (name)
+VALUES ('John Lasseter'),
+       ('Pete Docter'),
+       ('Andrew Stanton'),
+       ('Brad Bird'),
+       ('Lee Unkrich'),
+       ('Brenda Chapman'),
+       ('Dan Scanlon');
+
+-- dropp films table
+ALTER TABLE films
+ADD director_id INTEGER;
+
+-- re-create the films table with director_id column
+ CREATE TABLE IF NOT EXISTS films(
+  id SERIAL PRIMARY KEY,
+  title TEXT UNIQUE NOT NULL,
+  genre TEXT,
+  release_year INTEGER,
+  score INTEGER,
+  director_id INTEGER
+)
+
+-- adding records in to the films table.
+INSERT INTO films
+       (title, genre, release_year, score, director_id)
+VALUES ('The Shawshank Redemption', 'Drama', 1994, 9, 1),
+       ('The Godfather', 'Crime', 1972, 9, 1),
+       ('The Dark Knight', 'Action', 2008, 9, 1),
+       ('Alien', 'SciFi', 1979, 9, 2),
+       ('Total Recall', 'SciFi', 1990, 8, 3),
+       ('The Matrix', 'SciFi', 1999, 8, 4),
+       ('The Matrix Resurrections', 'SciFi', 2021, 5, 1),
+       ('The Matrix Reloaded', 'SciFi', 2003, 6, 4),
+       ('The Hunt for Red October', 'Thriller', 1990, 7, 3),
+       ('Misery', 'Thriller', 1990, 7, 2),
+       ('The Power Of The Dog', 'Western', 2021, 6, 5),
+       ('Hell or High Water', 'Western', 2016, 8, 1),
+       ('The Good the Bad and the Ugly', 'Western', 1966, 9, 6),
+       ('Unforgiven', 'Western', 1992, 7, 7);
+
+-- returns a list of films with their director
+SELECT title AS movie_name, name AS director
+FROM films
+INNER JOIN directors 
+    ON films.director_id = directors.id;
