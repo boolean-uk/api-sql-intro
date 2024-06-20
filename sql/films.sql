e3058af2d9c65034c8a80b492b425360452f18ea
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-20 11:36:48 +0200

    select different films

--- sql/films.sql
+++ sql/films.sql
@@ -25,3 +25,16 @@ VALUES
 ('The Good the Bad and the Ugly', 'Western', 1966, 9),
 ('Unforgiven', 'Western', 1992, 7) RETURNING *;
 COMMIT;
+
+SELECT * FROM films;
+SELECT * FROM films ORDER BY rating DESC;
+SELECT * FROM films ORDER BY release_year ASC;
+SELECT * FROM films WHERE rating > 7;
+SELECT * FROM films WHERE release_year = 1990;
+SELECT * FROM films WHERE release_year < 2000;
+SELECT * FROM films WHERE release_year BETWEEN 1990 AND 1999;
+SELECT * FROM films WHERE genre = 'SciFi';
+SELECT * FROM films WHERE genre = 'SciFi' OR genre = 'Western';
+SELECT * FROM films WHERE genre != 'SciFi';
+SELECT * FROM films WHERE genre = 'Western' AND release_year < 2000;
+SELECT * FROM films WHERE title LIKE '%Matrix%';
