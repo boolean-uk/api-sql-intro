da04f7b6c906ada29dac6dff36982dc88c2a859a
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-06-24 17:36:05 +0100

    standard criteria

--- films.sql
+++ films.sql
@@ -22,9 +22,15 @@ INSERT INTO films (title, genre, release_year, rating) VALUES ('The Good the Bad
 INSERT INTO films (title, genre, release_year, rating) VALUES ('Unforgiven', 'Western', 1992, 7);
 
 SELECT * FROM films;
-
 SELECT * FROM films ORDER BY rating DESC;
-
 SELECT * FROM films ORDER BY release_year ASC;
-
 SELECT * FROM films WHERE rating > 7;
+SELECT * FROM films WHERE release_year = 1990;
+SELECT * FROM films WHERE release_year < 2000;
+SELECT * FROM films WHERE release_year BETWEEN 1990 AND 1999;
+SELECT * FROM films WHERE genre = 'SciFi';
+SELECT * FROM films WHERE genre = 'SciFi' OR genre = 'Western';
+SELECT * FROM films WHERE genre != 'SciFi';
+SELECT * FROM films WHERE genre = 'Western' AND release_year < 2000;
+SELECT * FROM films WHERE title LIKE '%Matrix%';
+
